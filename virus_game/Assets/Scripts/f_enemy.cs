40fde8d99471256a6e0a29f3423a05776cbee461
piratemrs <piratemrs@gmail.com>
2019-01-22 16:09:41 +0200

    enemy2

--- virus_game/Assets/Scripts/f_enemy.cs
+++ virus_game/Assets/Scripts/f_enemy.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class f_enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 3 ;
+    public GameObject target;
 
+    private bool isDetected = false;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(target.GetComponent<Transform>().position, transform.position) < 3)
+        {
+            isDetected = true;
+        }
+        if(target != null && isDetected)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.GetComponent<Transform>().position, speed * Time.deltaTime);
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "player")
+        {
+            Destroy(gameObject);
+
+        }
+
+    }
+
 }

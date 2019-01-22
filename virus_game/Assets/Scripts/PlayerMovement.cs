7e9a745409f87c9c624e65fdc11490b1c197f680
ahmedsamir05 <ahmedsamir04@gmail.com>
2019-01-22 09:18:48 +0200

    scripts

---
+++ virus_game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    float speed;
+    Rigidbody2D body;
+    Vector3 velocity;
+    Vector3 rotation;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        velocity = new Vector3();
+        rotation = new Vector3();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity.x = -speed;
+            //transform.localScale = new Vector3(-1, 1, 1);
+            rotation.y = 180;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity.x = speed;
+            rotation.y = 0;
+        }
+        body.velocity = velocity;
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            velocity.y = -speed;
+            //transform.localScale = new Vector3(-1, 1, 1);
+            rotation.x = 180;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            velocity.y = speed;
+            rotation.x = 0;
+
+        }
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+}
